3d5635b770a7e2745fe07ad1d5825f79e703b8f4
Ross Gardiner <github@public.rossng.eu>
2025-04-25 00:38:27 +0200

    Header link to homepage

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,13 @@
+import { A } from '@solidjs/router';
 import type { Component } from 'solid-js';
 
 const Header: Component = () => {
   return (
     <header class="w-full p-4 text-center">
-      <h1 class="text-4xl font-extrabold">Merhaba</h1>
+      <A href="/">
+        {' '}
+        <h1 class="text-4xl font-extrabold">Merhaba</h1>
+      </A>
       <p>Turkish learning tools</p>
     </header>
   );
