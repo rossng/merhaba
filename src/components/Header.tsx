006abfac2dd101e50d79da0c4ad43d31da392aa4
Ross Gardiner <github@public.rossng.eu>
2025-04-25 23:00:25 +0200

    Add red stripe

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import type { Component } from 'solid-js';
 
 const Header: Component = () => {
   return (
-    <header class="w-full p-4 text-center">
+    <header class="w-full p-4 text-center" style={{ 'border-top': '4px solid #E30A17' }}>
       <A href="/">
         {' '}
         <h1 class="text-4xl font-extrabold">Merhaba</h1>
